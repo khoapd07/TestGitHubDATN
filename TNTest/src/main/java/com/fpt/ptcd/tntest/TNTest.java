4561db16a1eb4fd461c4cfdd99bf73a596c36ff6
Phạm Đăng Khoa <phamdangkhoa131207@gmail.com>
2025-03-20 16:38:59 +0700

    test push

---
+++ TNTest/src/main/java/com/fpt/ptcd/tntest/TNTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fpt.ptcd.tntest;
+
+/**
+ *
+ * @author phamd
+ */
+public class TNTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
